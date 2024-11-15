034496892d79d28f243cfb012ac9077524fb3dc1
Nick Bonakdarpour-Jaberg <286027@glenbrook225.org>
2024-11-15 08:59:25 -0600

    yj5

--- script.js
+++ script.js
@@ -72,14 +72,40 @@ function showNote() {
   }
 }
 
+//CALCULATOR (JAVASCRIPT)
 var showCalc = false;
 function showCalculator(){
- if (showCalc == false){
-     document.getElementById("calc").style.opacity = 1;
-     showCalc = true;
- }
- else if (showCalc == true){
-     document.getElementById("calc").style.opacity = 0;
-     showCalc = false;
- }
+   if (showCalc == false){
+       document.getElementById("calc").style.opacity = 1;
+       showCalc = true;
+   }
+   else if (showCalc == true){
+       document.getElementById("calc").style.opacity = 0;
+       showCalc = false;
+   }
 }
+
+if (localStorage["note"]) {
+  var note = localStorage["note"];
+  document.getElementById("note").value = note;
+}
+
+function autoSave() {
+  var note = document.getElementById("note").value;
+  localStorage.setItem("note", note);
+}
+
+//POST-IT NOTE SHOW AND HIDE
+var show = false;
+function showNote(){
+  if (show == false){
+      document.getElementById("note").style.opacity = 1;
+      show = true;
+  }
+  else if (show == true){
+      document.getElementById("note").style.opacity = 0;
+      show = false;
+  }
+}
+
+//MUST set “opacity:0” in CSS on #note
